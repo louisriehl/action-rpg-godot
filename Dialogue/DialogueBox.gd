041a54278701e0ef815e19708fd322be1f699fb8
lriehl <louisriehl@gmail.com>
2020-05-17 15:12:20 -0400

    connect signpost dialogue to dialogue box

--- Dialogue/DialogueBox.gd
+++ Dialogue/DialogueBox.gd
@@ -1,29 +1,29 @@
 extends Control
-class_name DialogueBox 
 
-var dialogue = {
-	"0": {"dialogue":"This is the first test dialogue line..."},
-	"1": {"dialogue":"This is the second line!"}
+enum {
+	STARTED,
+	STOPPED
 }
 
 onready var timer = $Timer
-var page = 0
 onready var panel = $Panel
 onready var dialogueBox = $Panel/HBoxContainer/Dialogue
 
-func _ready():
-	dialogueBox.text = dialogue[str(page)]["dialogue"]
+var page = 0
+var state = STOPPED
+var dialogue = {}
 	
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		if dialogue_line_not_completed():
-			dialogueBox.visible_characters = dialogue[str(page)]["dialogue"].length()
-		else: 
-			page += 1
-			if page >= dialogue.size():
-				panel.visible = false
-			else:
-				nextPage()
+	if state == STARTED:
+		if Input.is_action_just_pressed("ui_accept"):
+			if dialogue_line_not_completed():
+				dialogueBox.visible_characters = dialogue[str(page)]["dialogue"].length()
+			else: 
+				page += 1
+				if page >= dialogue.size():
+					panel.visible = false
+				else:
+					nextPage()
 
 func nextPage() -> void:
 	dialogueBox.visible_characters = 0
@@ -31,6 +31,10 @@ func nextPage() -> void:
 	
 func dialogue_line_not_completed() -> bool:
 	return dialogueBox.visible_characters < dialogue[str(page)]["dialogue"].length()
+	
+func _dialogue_Started( dialogue ):
+	self.visible = true
+	print(dialogue)
 	
 func _on_Timer_timeout():
 	dialogueBox.visible_characters += 1
