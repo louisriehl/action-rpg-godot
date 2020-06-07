7cf171cbc324882e793d89f92d89e0310482699d
lriehl <louisriehl@gmail.com>
2020-06-07 19:19:01 -0400

    can also spawn dragon enemies on defined spawns

--- ArenaArea.gd
+++ ArenaArea.gd
@@ -21,7 +21,8 @@ onready var triggerArea = $TriggerArea
 onready var waveCooldownTimer = $WaveCooldown 
 
 var blockers = []
-var spawners = []
+var bat_spawners = []
+var dragon_spawners = []
 var state = INACTIVE
 var current_wave = 0
 var enemies_alive_in_wave = 0
@@ -44,26 +45,39 @@ func _ready():
 		printerr("No waves defined for arena")
 	
 	blockers = get_node("ArenaBlockers").get_children()
-	spawners = get_node("ArenaSpawners").get_children()
+	bat_spawners = get_node("ArenaSpawners/BatSpawners").get_children()
+	dragon_spawners = get_node("ArenaSpawners/DragonSpawners").get_children()
 	waveCooldownTimer.set_wait_time(wave_cooldown)
 	
 func _physics_process(delta):
 	if state == WAVE_START:
 		var wave = waves[current_wave]
 		enemies_alive_in_wave = wave.size()
+		var bat_count = 0
+		var dragon_count = 0
 		for i in range(wave.size()):
-			spawn_enemy(wave[i], i)
+			if wave[i] == "BAT":
+				spawn_enemy(wave[i], bat_spawners, bat_count)
+				bat_count += 1
+			if wave[i] == "DRAGON":
+				spawn_enemy(wave[i], dragon_spawners, dragon_count)
+				dragon_count += 1
 		state = WAVE_UNDERWAY
 	
-func spawn_enemy(enemy, iterator):
-	var spawn_point = spawners[iterator]
+func spawn_enemy(enemy, spawns, iterator):
+	var spawn_point = spawns[iterator]
 	create_at_spawn(EnemySpawnEffect.instance(), spawn_point)
 	if enemy == "BAT":
 		var bat = Bat.instance()
 		bat.connect("died", self, "_spawned_enemy_died")
 		bat.player_detection_modifier = 3
 		create_at_spawn(bat, spawn_point)
-	
+	elif enemy == "DRAGON":
+		var dragon = Dragon.instance()
+		dragon.connect("died", self, "_spawned_enemy_died")
+		dragon.player_detection_modifier = 3
+		create_at_spawn(dragon, spawn_point)
+
 func create_at_spawn(entity, spawn):
 	var main = get_tree().current_scene
 	entity.global_position = spawn.global_position	
@@ -85,9 +99,10 @@ func _on_WaveCooldown_timeout():
 	
 func _spawned_enemy_died():
 	enemies_alive_in_wave -= 1
-	if current_wave == waves.size() - 1:
-		end_arena()
-	elif enemies_alive_in_wave <= 0:
-		state = WAVE_COOLDOWN
-		current_wave += 1
-		waveCooldownTimer.start()
+	if enemies_alive_in_wave <= 0:
+		if current_wave == waves.size() - 1:
+			end_arena()
+		else:
+			state = WAVE_COOLDOWN
+			current_wave += 1
+			waveCooldownTimer.start()
