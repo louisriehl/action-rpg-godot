959547e7402c11c6ec30f7ffd0b0c8cc33d25748
louisriehl <louisriehl@gmail.com>
2020-06-26 11:23:17 -0700

    added particles to heart pickup

--- Pickups/HeartPickup.gd
+++ Pickups/HeartPickup.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+onready var particles = $Particles2D
+onready var explosion = $Explosion
+
+func _ready():
+	explosion.emitting = true
+	particles.emitting = true
+	
 func _on_Area2D_body_entered(body):
 	PlayerStats.set_max_health( PlayerStats.max_health + 1)
 	PlayerStats.restore_all_health()
