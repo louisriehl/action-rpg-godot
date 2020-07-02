f929000d12deb1c1e3932bf17737eac12437979f
Louis Riehl <42213557+louisriehl@users.noreply.github.com>
2020-07-02 18:32:17 -0400

    added locks and keys for temple door (#10)
    * added key, particles
    
    initial effects for key and pickup
    
    * added door that unlocks after three keys used
    
    * added lock sprite
    
    * updated tileset for temple, expanded temple test
    
    * finished and tested shader
    
    * initial additions and cleanups

--- Utils/Stats.gd
+++ Utils/Stats.gd
@@ -1,13 +1,16 @@
 extends Node
 
 # This class is an autoloaded singleton (see project settings)
+const max_keys = 3
 
 export var max_health : int = 1 setget set_max_health
 var health : int = max_health setget set_health
+var keys : int = 0 setget set_keys
 
 signal no_health
 signal health_changed(value)
 signal max_health_changed(value)
+signal keys_changed(value)
 
 func _ready():
 	self.health = max_health
@@ -22,6 +25,10 @@ func set_max_health(value):
 	max_health = value
 	self.health = min(health, max_health )
 	emit_signal("max_health_changed", max_health)
+	
+func set_keys(value):
+	keys = min(value, max_keys)
+	emit_signal("keys_changed", keys)
 	
 func restore_all_health():
 	self.health = max_health
